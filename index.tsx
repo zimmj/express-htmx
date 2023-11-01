6726eea7c37f144cd2c1b219d47d259d7a493004
zimmj <33086958+zimmj@users.noreply.github.com>
2023-11-02 00:09:20 +0100

    ~feat(set-up) add tsx support and htmx

---
+++ index.tsx
@@ -0,0 +1,41 @@
+import express from "express";
+import dotenv from "dotenv";
+import * as elements from "typed-html";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
+
+const BaselHtml = ({ children }: elements.Children) => `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <script src="https://unpkg.com/htmx.org@1.9.6"></script>
+    <meta charset="UTF-8" />
+    <title>React SSR</title>
+  </head>
+  ${children}
+
+</html>
+`;
+
+app.get("/", (req, res) => {
+  res.send(
+    <BaselHtml>
+      <body>
+        <button hx-post="clicked" hx-swap="outerHTML"> CLick Me</button>
+      </body>
+    </BaselHtml>
+  );
+});
+
+app.post("/clicked", (req, res) => {
+  res.send(
+    <div>I'm from the server!</div>
+  );
+});
