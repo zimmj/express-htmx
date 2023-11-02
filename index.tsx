5625e90047904badb1507ea37c2c68d89dc96561
zimmj <33086958+zimmj@users.noreply.github.com>
2023-11-02 09:00:56 +0100

    ~feat(set-up): Add bad tailwind support need to run cli
    npx tailwindcss-cli build -o public/css/out-style.css
    
    to regenerate newest style.css

--- index.tsx
+++ index.tsx
@@ -11,31 +11,33 @@ app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
 
+app.use(express.static(__dirname + "/public"));
+
 const BaselHtml = ({ children }: elements.Children) => `
 <!DOCTYPE html>
 <html lang="en">
   <head>
-  <script src="https://unpkg.com/htmx.org@1.9.6"></script>
+    <script src="https://unpkg.com/htmx.org@1.9.6"></script>
+    <link rel="stylesheet" type="text/css" href="css/out-style.css" />
     <meta charset="UTF-8" />
     <title>React SSR</title>
   </head>
   ${children}
-
 </html>
 `;
 
 app.get("/", (req, res) => {
+
   res.send(
-    <BaselHtml>
+    < BaselHtml >
       <body>
         <button hx-post="clicked" hx-swap="outerHTML"> CLick Me</button>
       </body>
-    </BaselHtml>
-  );
+    </BaselHtml >)
 });
 
 app.post("/clicked", (req, res) => {
   res.send(
-    <div>I'm from the server!</div>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
   );
 });
