e2350bead9232893c48cf528c7eacbab3b585884
zimmj <33086958+zimmj@users.noreply.github.com>
2024-02-23 13:54:58 +0100

    ~feat(tutorial): Add swap events

---
+++ controllers/tutorial.controller.tsx
@@ -0,0 +1,27 @@
+import { Router } from "express";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as elements from "typed-html";
+import { Layout } from "..";
+import { swapExample } from "./tutorial/swap.controller";
+
+const tutorialRouter = Router();
+export default tutorialRouter;
+
+tutorialRouter.get("/index", (_, res) => {
+    res.send(
+        <Layout>
+            <div hx-get="/tutorial/example/swap" hx-swap="outerHTML" hx-trigger="load"></div>
+        </Layout>
+    )
+});
+
+
+tutorialRouter.get("/example/swap", (_, res) => {
+    swapExample(res);
+});
+
+tutorialRouter.get("/example/swap/load-text", (_, res) => {
+    res.send(
+        <p>Loaded text from get</p>
+    )
+});
